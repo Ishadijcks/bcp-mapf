0de51f00471d65788b3992c26b64f66d50b82f24
Isha Dijcks <ishadijcks@gmail.com>
2020-06-06 17:59:24 +0200

    Add empty bellman-ford class

---
+++ trufflehog/BellmanFord.cpp
@@ -0,0 +1,10 @@
+/*
+Author: Isha Dijcks <i.e.dijcks@student.tudelft.nl>
+*/
+
+#include "BellmanFord.h"
+
+namespace TruffleHog {
+
+
+}
---
+++ trufflehog/BellmanFord.h
@@ -0,0 +1,17 @@
+/*
+Author: Isha Dijcks <i.e.dijcks@student.tudelft.nl>
+*/
+
+#ifndef BCP_MAPF_BELLMANFORD_H
+#define BCP_MAPF_BELLMANFORD_H
+
+#include "AbstractPathfinder.h"
+
+namespace TruffleHog {
+    class BellmanFord : public AbstractPathfinder {
+
+    };
+
+}
+
+#endif //BCP_MAPF_BELLMANFORD_H
